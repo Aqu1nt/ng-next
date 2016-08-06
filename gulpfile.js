c4778ae9856989f9dee199ca1bb8737915d3fd92
Emil <emil97@gmx.ch>
2016-08-06 07:05:55 +0200

    npm changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var elixir = require("laravel-elixir");
 
 elixir(function(mix){
-    mix.browserify('./src/Angular2to1.js', './dist/ng-es7.min.js', '.', {
+    mix.browserify('./src/Angular2to1.js', './dist/angular-es7-integration.min.js', '.', {
         standalone : "Angular2to1",
         bare : true
     });
